f67c6d025de1a2d4b9e6bae4559f84d15266c73d
ShamimShahriyar <109415359+ShamimShahriyar@users.noreply.github.com>
2025-03-15 21:51:51 +0530

    Create test_deps.py

---
+++ test_deps.py
@@ -0,0 +1,10 @@
+import cv2
+import tensorflow as tf
+import keras
+import deepface
+import numpy as np
+import tqdm
+import tkinter
+import shutil
+
+print("✔️ All dependencies are installed correctly!")
